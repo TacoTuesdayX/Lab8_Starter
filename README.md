770aa2ff90f0f3cc88d157a86fb006a373e8800f
TacoTuesdayX <loganramos00@gmail.com>
2021-11-13 12:17:56 -0800

    explore portion complete.

--- README.md
+++ README.md
@@ -5,6 +5,13 @@
 - This would occur within a github action, or whatever CI pipeline is in use, we would have them run whenever code is pushed.
 
 2) Would you use an end to end test to check if a function is returning the correct output? (yes/no)
-- No, use a unit test for specific functions
 
-3)
+- No, use a unit test for specific functions.
+
+3) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+- No, it would be best to utilize end to end tests to validate this feature. A messaging system has many moving parts to it, and unit tests are primarily used for testing single pieces of code (ie functions).
+
+4) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+- Yes, this feature can be tested using a unit test. Simply construct mock messages, or test cases, which represent various user messages (some with >80, <80, or =80). And assert the behavior (block vs no-block) is what we expected given that input.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,93 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// for isPhoneNumber
+
+test('valid phone number 1, should be true', () => {
+    expect(functions.isPhoneNumber('818-333-3333')).toBe(true);
+});
+
+test('valid phone number 2, should be true', () => {
+    expect(functions.isPhoneNumber('424-424-2424')).toBe(true);
+});
+
+test('invalid phone number 1, should be false', () => {
+    expect(functions.isPhoneNumber('424555')).toBe(false);
+});
+
+test('invalid phone number 2, should be false', () => {
+    expect(functions.isPhoneNumber('hello world')).toBe(false);
+});
+
+// for isEmail
+
+test('valid email 1, should be true', () => {
+    expect(functions.isEmail('xman@gmail.com')).toBe(true);
+});
+
+test('valid email 2, should be true', () => {
+    expect(functions.isEmail('clifford@thebigreddog.us')).toBe(true);
+});
+
+test('invalid email 1, should be false', () => {
+    expect(functions.isEmail('you will never have my emaiil')).toBe(false);
+});
+
+test('invalid email 2, should be false', () => {
+    expect(functions.isEmail('888-888-8888')).toBe(false);
+});
+
+// for isStrongPassword
+
+test('checking strong password 1, should be true', () => {
+    expect(functions.isStrongPassword('L89_4xxxiJlm')).toBe(true);
+});
+
+test('checking strong password 2, should be true', () => {
+    expect(functions.isStrongPassword('a894xcSgaA')).toBe(true);
+});
+
+test('checking weak password 1, should be false', () => {
+    expect(functions.isStrongPassword('12345')).toBe(false);
+});
+
+test('checking weak password 2, should be false', () => {
+    expect(functions.isStrongPassword('Ane')).toBe(false);
+});
+
+// for isDate
+
+test('valid date 1, should be true', () => {
+    expect(functions.isDate('12/12/1212')).toBe(true);
+});
+
+test('valid date 2, should be true', () => {
+    expect(functions.isDate('05/21/2020')).toBe(true);
+});
+
+test('invalid date 1, should be false', () => {
+    expect(functions.isDate('999/99/9999')).toBe(false);
+});
+
+test('invalid date 2, should be false', () => {
+    expect(functions.isDate('Mar 5')).toBe(false);
+});
+
+// for isHexColor
+
+test('valid hex color 1, should be true', () => {
+    expect(functions.isHexColor('#9AFFFB')).toBe(true);
+});
+
+test('valid hex color 2, should be true', () => {
+    expect(functions.isHexColor('#FFFFFF')).toBe(true);
+});
+
+test('invalid hex color 1, should be false', () => {
+    expect(functions.isHexColor('yellow')).toBe(false);
+});
+
+test('invalid hex color 2, should be false', () => {
+    expect(functions.isHexColor('#5252')).toBe(false);
+});
